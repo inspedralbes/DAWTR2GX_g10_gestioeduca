2f2f15485f1b01d1cdc6b4e251757a69e42fe90f
LucasBenitez10 <a23lucbensoj@inspedralbes.cat>
2025-01-08 09:03:39 +0100

    feat: Added new table for options of forms

---
+++ backend/app/Models/Option.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Option extends Model
+{
+    public function question(){
+        return $this->belongsTo(Question::class,'question_id');
+    }
+}
