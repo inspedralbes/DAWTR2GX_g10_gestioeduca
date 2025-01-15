9013ebc8b7e752bd9ed9d27f9b4d4400498dff9a
adriapedralbes <a23adrestmar@inspedralbes.cat>
2025-01-15 04:01:59 +0100

    arreglo rutas grupos manual

--- frontend/src/views/DashboardView.vue
+++ frontend/src/views/DashboardView.vue
@@ -12,9 +12,9 @@ const menuItems = [
     route: '/alumnos'
   },
   {
-    title: 'Gestió de Grups',
+    title: 'Grups',
     icon: 'M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z',
-    route: '/grupos'
+    route: '/manualCreateGroup'
   },
   {
     title: 'Formularis',
@@ -30,6 +30,11 @@ const menuItems = [
     title: 'Chat IA',
     icon: 'M8.5 2a1 1 0 000 2h2.086a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2H7a2 2 0 01-2-2V4a2 2 0 012-2h1.5z',
     route: '/brain'
+  },
+  {
+    title: 'Logout',
+    icon: 'M17 13l4-4m0 0l-4-4m4 4H3', 
+    route: '/tancar-sessio'
   }
 ];
 
